d88dc358a7057befdbb754a2c786e8cde497d3bd
Sandra Froberg Birkefeldt <sandrafrobergbirkefeldt@Sandras-MBP.emnet.dk>
2020-03-15 17:01:03 +0100

    version 2 - 15032020

--- java/javascript.js
+++ java/javascript.js
@@ -1,1 +1,12 @@
 'use strict'
+// UDSTYR LISTE
+function udstyr {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
