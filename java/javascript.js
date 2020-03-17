1b8cad3ba16c4e6e4a766fc8fb67676c11418ef2
Sandra Froberg Birkefeldt <sandrafrobergbirkefeldt@Sandras-MBP.emnet.dk>
2020-03-17 20:40:08 +0100

    version 6 - 17032020

--- java/javascript.js
+++ java/javascript.js
@@ -10,3 +10,15 @@ function udstyr {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
+    // Tidszone for Danmark
+    fetch("http://worldtimeapi.org/api/timezone/Europe/Copenhagen")
+
+        .then(function (data) {
+            return data.json();
+        })
+
+        .then(function (post) {
+            document.getElementById("tid").innerHTML = post.utc_datetime + post.weather[0].description;
+
+        })
